5aa04edd5d4489fa0db86c68f71fdbee1293fd95
manoj <basnetmanoj557@gmail.com>
2026-02-21 18:30:07 +0545

    Add employee management functionality
    - Introduced `All_in_One` class with properties for ID, Name, and Age, along with a Display method.
    - Created `PartTime` class inheriting from `All_in_One`, adding a Salery property and DisplaySalery method.
    - Updated `Program.cs` to replace e-commerce logic with a menu for adding and displaying employee details.
    - Simplified user interaction to focus on employee management, allowing continuous input until exit.

---
+++ Simple E-Commerce Cart System/All_in_One.cs
@@ -0,0 +1,25 @@
+﻿public class All_in_One
+{
+    public int ID { get; set; }
+
+    private string name;
+    private int age;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Name of person: " + name);
+        Console.WriteLine("Age of person: " + age);
+    }
+}
---
+++ Simple E-Commerce Cart System/PartTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_E_Commerce_Cart_System
+{
+    public class PartTime : All_in_One
+    {
+        public float Salery { get; set; }
+
+        public void DisplaySalery()
+        {
+            Display();
+
+            Console.WriteLine("your salery is "+ Salery);
+
+
+        }
+
+
+    }
+}
